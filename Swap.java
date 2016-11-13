61ff3de7d87ea75ca5519c75d56e37cdbb1fb901
Abhishek Lomsh <lomshabhishek@gmail.com>
2016-11-14 01:07:25 +0530

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Swap{
+
+swapf(int a,int b){
+int temp = a;
+a = b;
+b = temp
+}
+
+public static void main(String args[]){
+System.out.println("Welcome! "+\n+"Please enter two numbers to switch");
+Scanner obj = new Scanner(System.in);
+int a = obj.nextint();
+int b = obj.nextint();
+swapf(a,b);
+System.out.println("First number is "+a+\n+"Second number is "+b); 
+
+
+}
